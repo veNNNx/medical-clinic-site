c1e8180c6499e6a389680cf4caabcd246d751532
veNNN <piotrek.cwalina1@wp.pl>
2022-11-18 15:02:19 +0100

    Refactor, add User as foreign key

--- main/models.py
+++ main/models.py
@@ -3,40 +3,46 @@ from django.contrib.auth.models import User
 import datetime
 # Create your models here.
 
+
 class Post(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length=100)
     text = models.TextField()
-    date_created = models.DateTimeField(auto_now_add = True)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title 
+        return self.title
+
 
 class Visit(models.Model):
-    doctor = models.IntegerField()
+    doctor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='visit_doctor')
     date = models.DateField()
-    time = models.TimeField(default=datetime.time(0,0))
-    user_id = models.TextField()
+    time = models.TimeField(default=datetime.time(0, 0))
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='visit_user')
 
     def __str__(self):
-        return str(self.doctor) + ' ' + str(self.date) + ' ' + str(self.time)
+        return str(self.doctor.first_name) + ' ' + str(self.doctor.last_name) + ' ' + str(self.user.first_name) + ' ' + str(self.user.last_name) + ' ' + str(self.date) + ' ' + str(self.time)
+
 
 class Profile(models.Model):
-    user_id = models.IntegerField()
-    date_created = models.DateTimeField(auto_now_add = True)
-    date_modified = models.DateTimeField(auto_now_add = True)
-    is_doctor = models.BooleanField(default = False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='profile_user')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+    is_doctor = models.BooleanField(default=False)
     # if is doctor
     desc = models.TextField(blank=True)
     spec = models.TextField(blank=True)
-    img = models.ImageField(upload_to='images/', default='images/default_picture.jpg')
+    img = models.ImageField(upload_to='images/',
+                            default='images/default_picture.jpg')
     # if not
-    pesel = models.IntegerField(default = 0)
-    tel = models.IntegerField(default = 0, blank=True)
-    city = models.CharField(max_length = 100, blank=True)
-    zip_code = models.CharField(max_length = 6, blank=True)
-    street = models.CharField(max_length = 100, blank=True)
-    addr_number = models.CharField(max_length = 100, blank=True)
-    
+    pesel = models.IntegerField(default=0)
+    tel = models.IntegerField(default=0, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    zip_code = models.CharField(max_length=6, blank=True)
+    street = models.CharField(max_length=100, blank=True)
+    addr_number = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return str(self.user_id) + ' ' + str(self.is_doctor)
+        return str(self.user_id) + ' ' + str(self.user.first_name) + ' ' + str(self.user.last_name) + ' ' + str(self.is_doctor)
--- main/views.py
+++ main/views.py
@@ -9,18 +9,21 @@ import datetime
 import json
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home.html',)
+
 
 def advertisements(request):
     all_posts = Post.objects.order_by('-date_created')
     if request.method == 'POST':
         post_id = request.POST.get('detele_post_id')
-        post = Post.objects.filter(id = post_id).first()
+        post = Post.objects.filter(id=post_id).first()
         if post:
             post.delete()
 
     return render(request, 'advertisements.html', {'all_posts': all_posts})
+
 
 def sign_up(request):
     if request.method == 'POST':
@@ -28,33 +31,35 @@ def sign_up(request):
         profile_form = ProfileForm()
         if form.is_valid():
             user = form.save()
-            Profile.objects.create(user_id = user.id)
+            Profile.objects.create(user=user)
             login(request, user)
             return redirect('/home')
     else:
         form = RegisterForm()
 
-    return render(request, 'registration/sign_up.html', {'form':form})
+    return render(request, 'registration/sign_up.html', {'form': form})
 
 # region DOCTOR
+
+
 def is_doctor(user):
-    return user.groups.filter(name='Doctor_perm').exists()
+    profile = Profile.objects.get(user=user)
+    return profile.is_doctor
+
 
 @login_required(login_url='home.html')
 @user_passes_test(is_doctor)
 def profile(request, id):
-    next_visits = Visit.objects.filter(user_id = id, 
-                            date__gte=datetime.datetime.today()).order_by('-date', '-time')
-    past_visits = Visit.objects.filter(user_id = id, 
-                            date__lt=datetime.datetime.today()).order_by('-date', '-time')
-
-    next_visits = change_doctor_info(next_visits)
-    past_visits = change_doctor_info(past_visits)
-
     user = User.objects.get(id=id)
-    profile = Profile.objects.get(user_id = id)
+    profile = Profile.objects.get(user=user)
+
+    next_visits = Visit.objects.filter(user=user,
+                                       date__gte=datetime.datetime.today()).order_by('-date', '-time')
+    past_visits = Visit.objects.filter(user=user,
+                                       date__lt=datetime.datetime.today()).order_by('-date', '-time')
 
     return render(request, 'doctor/profile.html', {'user': user, 'profile': profile, 'past_visits': past_visits, 'next_visits': next_visits})
+
 
 @login_required(login_url='home.html')
 @user_passes_test(is_doctor)
@@ -66,42 +71,43 @@ def create_post(request):
             return redirect('/home')
     else:
         form = PostForm()
-    
-    return render(request, 'doctor/create_post.html', {'form':form})
+
+    return render(request, 'doctor/create_post.html', {'form': form})
+
 
 @login_required(login_url='home.html')
 @user_passes_test(is_doctor)
 def patients(request):
-    users = User.objects.filter(groups__isnull=True)
-    return render(request, 'doctor/patients.html', {'users':users})
+    profiles = Profile.objects.filter(is_doctor=False)
+    users = []
+    for prof in profiles:
+        users.append(prof.user)
+
+    return render(request, 'doctor/patients.html', {'users': users})
+
 
 @login_required(login_url='home.html')
 @user_passes_test(is_doctor)
 def schedule(request):
     if request.method == 'POST':
-        date=request.POST.get('date')
-        visits = Visit.objects.filter(doctor = request.user.id, date=date)
-        visits.order_by('-time')
-        for v in visits:
-            patient = User.objects.filter(id = v.user_id)
-            v.user_id = patient[0]
-        print(date, visits)
-        return render(request, 'doctor/schedule.html', {'date': date, 'visits':visits})
+        date = request.POST.get('date')
+        visits = Visit.objects.filter(
+            doctor=request.user, date=date).order_by('-time')
+        return render(request, 'doctor/schedule.html', {'date': date, 'visits': visits})
     else:
         date = datetime.date.today().strftime('%Y-%m-%d')
-        visits = Visit.objects.filter(doctor = request.user.id, date=date)
-        visits.order_by('date')
-        for v in visits:
-            patient = User.objects.filter(id = v.user_id)
-            v.user_id = patient[0]
-        return render(request, 'doctor/schedule.html', {'date': date, 'visits':visits, 'default': True})
+        visits = Visit.objects.filter(
+            doctor=request.user, date=date).order_by('date')
+        return render(request, 'doctor/schedule.html', {'date': date, 'visits': visits, 'default': True})
 
-#endregion
+# endregion
 
 # region PATIENT
+
+
 @login_required(login_url='home.html')
 def my_profile(request):
-    profile = Profile.objects.get(user_id=request.user.id)
+    profile = Profile.objects.get(user=request.user)
     if request.method == 'POST':
         profile.tel = request.POST.get('tel')
         if profile.is_doctor:
@@ -124,29 +130,30 @@ def my_profile(request):
 
 @login_required(login_url='home.html')
 def my_visits(request):
-    next_visits = Visit.objects.filter(user_id = request.user.id, 
-                                        date__gt=datetime.datetime.today()).order_by('-date', '-time')
-    today_visits = Visit.objects.filter(user_id = request.user.id, 
+    next_visits = Visit.objects.filter(user=request.user,
+                                       date__gt=datetime.datetime.today()).order_by('-date', '-time')
+    today_visits = Visit.objects.filter(user=request.user,
                                         date=datetime.datetime.today()).order_by('-time')
-    past_visits = Visit.objects.filter(user_id = request.user.id, 
-                                        date__lt=datetime.datetime.today()).order_by('-date', '-time')
+    past_visits = Visit.objects.filter(user=request.user,
+                                       date__lt=datetime.datetime.today()).order_by('-date', '-time')
 
-    next_visits = change_doctor_info(next_visits)
-    today_visits = change_doctor_info(today_visits)
-    past_visits = change_doctor_info(past_visits)
+    return render(request, 'patient/my_visits.html', {'next_visits': next_visits, 'past_visits': past_visits, 'today_visits': today_visits})
 
-    return render(request, 'patient/my_visits.html', {'next_visits':next_visits, 'past_visits':past_visits, 'today_visits':today_visits})
 
 @login_required(login_url='home.html')
 def new_visit(request):
-    doctors = User.objects.filter(groups__name='Doctor_perm')
-    profiles =  Profile.objects.filter(is_doctor = True)
-    return render(request, 'patient/new_visit.html', {'doctors':doctors, 'profiles':profiles})
+    # doctors = User.objects.filter(groups__name='Doctor_perm')
+    profiles = Profile.objects.filter(is_doctor=True)
+    doctors = []
+    for prof in profiles:
+        doctors.append(prof.user)
+    return render(request, 'patient/new_visit.html', {'doctors': doctors, 'profiles': profiles})
+
 
 @login_required(login_url='home.html')
 def select_time(request, id):
+    doctor = User.objects.get(id=id)
     if request.method == 'POST':
-
         booking = request.POST.get('booking').split(',')
 
         _date = booking[0].split('-')
@@ -155,15 +162,15 @@ def select_time(request, id):
         _time = booking[1].split(':')
         time = datetime.time(int(_time[0]), int(_time[1]))
 
-        visit = Visit.objects.filter(doctor = id, date = date, time = time)
+        visit = Visit.objects.filter(doctor=doctor, date=date, time=time)
         if visit.exists():
             messages.error(request, 'Visit aslready taken, book another time!')
             return redirect(f'/new-visit/{id}')
 
         _mutable = request.POST._mutable
         request.POST._mutable = True
-        request.POST['user_id'] = request.user.id
-        request.POST['doctor'] = id
+        request.POST['user'] = request.user
+        request.POST['doctor'] = doctor
         request.POST['time'] = time
         request.POST['date'] = date
         request.POST._mutable = _mutable
@@ -174,39 +181,33 @@ def select_time(request, id):
             messages.success(request, 'Visit booked succesfullty!')
             return redirect('/my-visits')
     elif request.method == 'GET':
-        visits = Visit.objects.filter(doctor = id, date__gte=datetime.datetime.today())
+        visits = Visit.objects.filter(
+            doctor=doctor, date__gte=datetime.datetime.today())
         booked_time = []
         for vis in visits:
             vis.time = vis.time.strftime('%H:%M')
             booked_time.append(f'{vis.date}{vis.time}')
-        
+
         working_time = WORKING_TIME.copy()
-        title = [(datetime.datetime.today() + datetime.timedelta(days=i)).strftime('%A %Y-%m-%d') for i in range(7)]
-        date = [(datetime.datetime.today() + datetime.timedelta(days=i)).strftime('%Y-%m-%d') for i in range(7)]
-        return render(request, 'patient/select_time.html', {'visits':visits, 'working_time':working_time, 'title':title, 'date':date, 'booked_time':booked_time})
+        title = [(datetime.datetime.today() + datetime.timedelta(days=i)
+                  ).strftime('%A %Y-%m-%d') for i in range(7)]
+        date = [(datetime.datetime.today() + datetime.timedelta(days=i)
+                 ).strftime('%Y-%m-%d') for i in range(7)]
+        return render(request, 'patient/select_time.html', {'visits': visits, 'working_time': working_time, 'title': title, 'date': date, 'booked_time': booked_time})
     return redirect('/new-visit')
 
-WORKING_TIME=['08:00', '08:30',
-            '09:00', '09:30',
-            '10:00', '10:30',
-            '11:00', '11:30',
-            '12:00', '12:30',
-            '13:00', '13:30',
-            '14:00', '14:30',
-            '15:00', '15:30',
-            '16:00', '16:30',
-            '17:00', '17:30']
 
-def change_doctor_info(visits):
-    for v in visits:
-        v.doctor = User.objects.filter(id = v.doctor)
-        v.doctor = v.doctor[0]
-    return visits
-#endregion
+WORKING_TIME = ['08:00', '08:30',
+                '09:00', '09:30',
+                '10:00', '10:30',
+                '11:00', '11:30',
+                '12:00', '12:30',
+                '13:00', '13:30',
+                '14:00', '14:30',
+                '15:00', '15:30',
+                '16:00', '16:30',
+                '17:00', '17:30']
+# endregion
 
-# profil - pakiet?, recepty json, sms, 
+# profil - pakiet?, recepty json, sms,
 # lekarz ustawia swoje godzinki + urlopy
-
-
-
-
